6f78132679ef99f37fe14a2339bd889f84a69110
CanekTorralba <canek.torralba@gmail.com>
2022-07-11 11:49:00 -0600

    Add Password Comparison Method - Attempt 1

--- src/models/user.js
+++ src/models/user.js
@@ -44,4 +44,16 @@ userSchema.pre('save', function (next) {
   }
 });
 
+//Compare password method. Attempt #1. Talk with Backend for its use.
+userSchema.methods.comparePassword = async function (password) {
+  if (!password) throw new Error('Password is missing, can not compare');
+
+  try {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+  } catch (error) {
+    console.log('Error while comparing password!', error.message);
+  }
+};
+
 module.exports = mongoose.model('User', userSchema);
